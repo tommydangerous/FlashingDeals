39cfb2310d2904a8a26009ba5c2418157fb2a7bf
QuantumVenturess <quantumventuress@gmail.com>
2012-01-24 14:56:46 -0800

    Changed mailer to not show http://

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ FlashingDeal::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => "http://www.flashingdeals.com" }
+  config.action_mailer.default_url_options = { :host => "www.flashingdeals.com" }
   
   ActionMailer::Base.default :from => 'support@flashingdeals.com'
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "http://www.flashingdeals.com",
+  :domain               => "www.flashingdeals.com",
   :user_name            => "support@flashingdeals.com",
   :password             => "ereinion05",
   :authentication       => "plain",
