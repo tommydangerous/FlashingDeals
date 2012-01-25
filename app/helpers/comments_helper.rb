2fcf83fd3e281f9a70e307ee31f81592f61baaf8
QuantumVenturess <quantumventuress@gmail.com>
2012-01-25 10:23:09 -0800

    made a#comment_index_at_deal font size 12px, changed the color to rgba(150, 150, 150, 1), auto_link comments, subcomments, messages, wrap comments, wrap_sub subcomments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,9 +4,20 @@ module CommentsHelper
   	sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
   end
   
+  def wrap_sub(content)
+  	sanitize(raw(content.split.map{ |s| wrap_long_string_sub(s) }.join(' ')))
+  end
+  
   private
   
-    def wrap_long_string(text, max_width = 110)
+    def wrap_long_string(text, max_width = 115)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text :
+      							  text.scan(regex).join(zero_width_space)
+    end
+    
+    def wrap_long_string_sub(text, max_width = 90)
       zero_width_space = "&#8203;"
       regex = /.{1,#{max_width}}/
       (text.length < max_width) ? text :
