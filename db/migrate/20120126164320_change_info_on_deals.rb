f37f9af9cc66df01c41b0ed28f71ffec99745053
Tommy Dang <QuantumVenturess@gmail.com>
2012-01-28 07:11:15 -0800

    Delete files

--- db/migrate/20120126164320_change_info_on_deals.rb
+++
@@ -1,8 +0,0 @@
-class ChangeInfoOnDeals < ActiveRecord::Migration
-  def change
-  	change_column :deals, :info, :text
-  	change_column :messages, :content, :text
-  	change_column :subcomments, :content, :text
-  	change_column :comments, :content, :text
-  end
-end
