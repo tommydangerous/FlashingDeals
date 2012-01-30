15c9b8f0870990e823f91a464831e0423f0a21d7
QuantumVenturess <quantumventuress@gmail.com>
2012-01-30 11:40:29 -0800

    deny access now redirects to the signup page instead of the login page, the signup page now has Already a member? Login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,7 +33,8 @@ module SessionsHelper
 	
 	def deny_access
 		store_location
-		redirect_to login_path, :notice => "Please login to access this page."
+		redirect_to signup_path
+#		redirect_to login_path, :notice => "Please login to access this page."
 	end
 	
 	def redirect_back_or(default)
