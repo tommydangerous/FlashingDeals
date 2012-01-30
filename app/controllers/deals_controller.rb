7ab5d7759fe822223f1115d058cb212f74588b68
Tommy Dang <QuantumVenturess@gmail.com>
2012-01-29 18:58:19 -0800

    redid make_remove.js.erb

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -291,6 +291,7 @@ class DealsController < ApplicationController
 				redirect_to queue_path
 			}
 			format.js {
+				@deal = deal
 				@deals = Deal.where("queue = ?", true).order("updated_at DESC")
 				@top_deals = Deal.where("top_deal = ?", true).order("updated_at DESC")
 			}
