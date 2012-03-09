c2bdc5d943876bfa28d6fa634df53f60af4be3e1
QuantumVenturess <quantumventuress@gmail.com>
2012-03-09 12:31:23 -0800

    created wrap for box deal and wrap for box deal large comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -8,6 +8,14 @@ module CommentsHelper
   	sanitize(raw(content.split.map{ |s| wrap_long_string_sub(s) }.join(' ')))
   end
   
+  def wrap_box_deal(content)
+  	sanitize(raw(content.split.map{ |s| wrap_long_string_box_deal(s) }.join(' ')))
+  end
+  
+  def wrap_box_deal_large(content)
+  	sanitize(raw(content.split.map{ |s| wrap_long_string_box_deal_large(s) }.join(' ')))
+  end
+  
   private
   
     def wrap_long_string(text, max_width = 115)
@@ -18,6 +26,20 @@ module CommentsHelper
     end
     
     def wrap_long_string_sub(text, max_width = 90)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text :
+      							  text.scan(regex).join(zero_width_space)
+    end
+    
+    def wrap_long_string_box_deal(text, max_width = 20)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text :
+      							  text.scan(regex).join(zero_width_space)
+    end
+    
+    def wrap_long_string_box_deal_large(text, max_width = 30)
       zero_width_space = "&#8203;"
       regex = /.{1,#{max_width}}/
       (text.length < max_width) ? text :
