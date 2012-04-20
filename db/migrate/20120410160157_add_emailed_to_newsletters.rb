d70e2e4eb23f022ac2f2027f524fdb063a74f663
QuantumVenturess <quantumventuress@gmail.com>
2012-04-19 19:59:14 -0700

    removed comments from deal show and deal show overlay

--- db/migrate/20120410160157_add_emailed_to_newsletters.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailedToNewsletters < ActiveRecord::Migration
-  def change
-    add_column :newsletters, :emailed, :boolean, :default => false
-    add_index :newsletters, :emailed
-  end
-end
--- db/migrate/20120410202713_add_monthly_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddMonthlyToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :monthly, :boolean, :default => false
-    add_index :users, :monthly
-  end
-end
