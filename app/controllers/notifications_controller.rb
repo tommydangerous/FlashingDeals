934108f6d918fdbdfe0b13de252b9d800a6d7f48
QuantumVenturess <quantumventuress@gmail.com>
2012-05-07 14:13:14 -0700

    added .take(100) to @notifications for index action in notifications_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
 	def index
 		@title = "Notifications"
 		@notifications = current_user.notifications.where("read = ? OR created_at > ?", false, (Time.now - (86400 * 14)))
-		@notifications_days = @notifications.group_by(&:month_day)
+		@notifications_days = @notifications.take(100).group_by(&:month_day)
 		current_user.notifications.where("read = ?", false).each do |n|
 			n.update_attribute(:read, true)
 		end
