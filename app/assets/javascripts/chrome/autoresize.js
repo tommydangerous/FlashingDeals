3b5695fb7ca1e8db5ab23e7c48612de30c307e06
QuantumVenturess <quantumventuress@gmail.com>
2012-07-06 13:34:02 -0700

    fixed autoresize.js for #comment_form for box deals

--- app/assets/javascripts/chrome/autoresize.js
+++ app/assets/javascripts/chrome/autoresize.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#comment_content').autoResize();
+	$('#comment_form #comment_content').autoResize();
 	$('.subcomment_textarea').autoResize();
 	$('.subcomment_textarea_2').autoResize();
 	$('textarea#message_content').autoResize();
--- app/assets/javascripts/ie-9/autoresize.js
+++ app/assets/javascripts/ie-9/autoresize.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#comment_content').autoResize();
+	$('#comment_form #comment_content').autoResize();
 	$('.subcomment_textarea').autoResize();
 	$('.subcomment_textarea_2').autoResize();
 	$('textarea#message_content').autoResize();
@@ -7,4 +7,7 @@ $(document).ready(function() {
 	$('textarea#deal_info').autoResize();
 	$('textarea#deal_name').autoResize();
 	$('textarea#deal_info').autoResize();
+	$('textarea#newsletter_info1').autoResize();
+	$('textarea#newsletter_info2').autoResize();
+	$('textarea#newsletter_info3').autoResize();
 });
