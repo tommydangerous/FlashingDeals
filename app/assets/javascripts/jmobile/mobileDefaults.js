034397c818e088cbf041fe5d2f096cfa0c7686c4
QuantumVenturess <quantumventuress@gmail.com>
2012-08-05 16:41:15 -0700

    when swiping deal show left or right, page transitions into next or previous deal

--- app/assets/javascripts/jmobile/mobileDefaults.js
+++ app/assets/javascripts/jmobile/mobileDefaults.js
@@ -3,4 +3,7 @@ $(document).bind("mobileinit", function() {
 	$.mobile.defaultPageTransition = 'none';
 	$.support.touchOverflow = true;
 	$.mobile.touchOverflowEnabled = true;
+	$.event.special.swipe.durationThreshold = 500;
+	$.event.special.swipe.horizontalDistanceThreshold = 60;
+	$.event.special.swipe.verticalDistanceThreshold = 20;
 })
--- app/assets/javascripts/jmobile/swipe.js
+++ app/assets/javascripts/jmobile/swipe.js
@@ -1,33 +1,39 @@
-/*
 $(document).on('pageinit', function() {
 	// swipe left and swipe right on deal list page or deal show page
 	$('.dealShowContent').on('swipeleft swiperight', function(event) {
-		if (event.type == 'swipeleft') {
+		if (event.type == 'swiperight') {
 			event.stopImmediatePropagation();
-			var next = $('.contentWrapper:last .pagination a.next_page');
-			var nextDeal = $('.contentWrapper:last .nextDeal');
-			if (next.length > 0) {
-				var nurl = next.attr('href');
-				$.mobile.changePage(nurl, { transition: "slide" });
-			}
-			if (nextDeal.length > 0) {
-				var nextDealUrl = nextDeal.attr('href');
-				$.mobile.changePage(nextDealUrl, { transition: "slide" });
-			}
-		}
-		else if (event.type == 'swiperight') {
-			event.stopImmediatePropagation();
-			var prev = $('.contentWrapper:last .pagination a.previous_page');
 			var prevDeal = $('.contentWrapper:last .prevDeal');
-			if (prev.length > 0) {
-				var purl = prev.attr("href");
-				$.mobile.changePage(purl, { transition: "slide", reverse: true });
-			}
 			if (prevDeal.length > 0) {
 				var prevDealUrl = prevDeal.attr('href');
-				$.mobile.changePage(prevDealUrl, { transition: "slide", reverse: true });
+				$.mobile.changePage(prevDealUrl);
+				if ($('.contentWrapper').length == 1) {
+					$('.contentWrapper #dealShow').toggle();
+					$('.contentWrapper #dealLoading').toggle();
+				}
+				else {
+					$('.contentWrapper:last #dealShow').hide();
+					$('.contentWrapper:last #dealLoading').show();
+					$(window).scrollTop(0);
+				}
+			}
+		}
+		else if (event.type == 'swipeleft') {
+			event.stopImmediatePropagation();
+			var nextDeal = $('.contentWrapper:last .nextDeal');
+			if (nextDeal.length > 0) {
+				var nextDealUrl = nextDeal.attr('href');
+				$.mobile.changePage(nextDealUrl);
+				if ($('.contentWrapper').length == 1) {
+					$('.contentWrapper #dealShow').toggle();
+					$('.contentWrapper #dealLoading').toggle();
+				}
+				else {
+					$('.contentWrapper:last #dealShow').hide();
+					$('.contentWrapper:last #dealLoading').show();
+					$(window).scrollTop(0);
+				}
 			}
 		}
 	})
 })
-*/
