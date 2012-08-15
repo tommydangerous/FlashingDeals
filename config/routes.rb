ab49e61c51a40ae4e2874cc3d34a71c92859043d
QuantumVenturess <quantumventuress@gmail.com>
2012-08-15 09:52:17 -0700

    removed subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ FlashingDeal::Application.routes.draw do
 	match '/google/access' => 'authentications#google_access', :as => :google_access
 	match '/google/auth' => 'authentications#google_auth', :as => :google_auth
 # Blogs
-	match '', to: 'blogs#index', constraints: {subdomain: /.+/}
+#	match '', to: 'blogs#index', constraints: {subdomain: /.+/}
 # Deals
   root :to => 'deals#featured_deals'
   match '/featured' => 'deals#featured_deals', :as => :featured_deals
