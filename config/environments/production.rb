729f7c80217725a2354dedec6918dba88dee0cb1
QuantumVenturess <quantumventuress@gmail.com>
2012-08-25 08:04:17 -0700

    added CDN asset host; added shadow for blog posts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@ FlashingDeal::Application.configure do
 
   # Rack::Cache Memcache
   config.cache_store = :dalli_store
-  config.action_dispatch.rack_cache = {
-    :metastore    => Dalli::Client.new,
-    :entitystore  => 'file:tmp/cache/rack/body',
-    :allow_reload => false
-  }
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=2592000"
+  # config.action_dispatch.rack_cache = {
+  #  :metastore    => Dalli::Client.new,
+  #  :entitystore  => 'file:tmp/cache/rack/body',
+  #  :allow_reload => false
+  #}
+  config.serve_static_assets = false
+  # config.static_cache_control = "public, max-age=2592000"
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -50,7 +50,7 @@ FlashingDeal::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
